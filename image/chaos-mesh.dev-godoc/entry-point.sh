bb53a3f95f72a0165e72d6d0daf1eceffe74f635
STRRL <str_ruiling@outlook.com>
2022-01-16 17:46:01 +0800

    chore: fix working directory
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- image/chaos-mesh.dev-godoc/entry-point.sh
+++ image/chaos-mesh.dev-godoc/entry-point.sh
@@ -7,7 +7,7 @@ echo "target branch: $TARGET_BRANCH_OR_TAG"
 echo "current directory: $(pwd)"
 git clone --depth 1 --branch $TARGET_BRANCH_OR_TAG https://github.com/chaos-mesh/chaos-mesh
 
-GIT_COMMIT=$(git log --pretty=format:'%h' -n 1)
+GIT_COMMIT=$(cd chaos-mesh && git log --pretty=format:'%h' -n 1)
 
 OUT_DIRECTORY="$(pwd)/out/$TARGET_BRANCH_OR_TAG"
 mkdir -p $OUT_DIRECTORY
