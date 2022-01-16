9870d2b27dd7b0f7f1ae75b70f2fbd979e4be388
STRRL <str_ruiling@outlook.com>
2022-01-16 17:44:44 +0800

    chore: add commit
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- image/chaos-mesh.dev-godoc/entry-point.sh
+++ image/chaos-mesh.dev-godoc/entry-point.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 TARGET_BRANCH_OR_TAG="${1:-master}"
-TITLE="${TITLE:-Chaos Mesh}"
-DESCRIPTION="${DESCRIPTION:-Updated at $(date)}"
+
 
 echo "target branch: $TARGET_BRANCH_OR_TAG"
 echo "current directory: $(pwd)"
 git clone --depth 1 --branch $TARGET_BRANCH_OR_TAG https://github.com/chaos-mesh/chaos-mesh
 
+GIT_COMMIT=$(git log --pretty=format:'%h' -n 1)
+
 OUT_DIRECTORY="$(pwd)/out/$TARGET_BRANCH_OR_TAG"
 mkdir -p $OUT_DIRECTORY
-cd chaos-mesh && gopages -brand-title ${TITLE} -brand-description ${DESCRIPTION} -base https://chaos-mesh.dev/godoc/$TARGET_BRANCH_OR_TAG -out $OUT_DIRECTORY
+
+TITLE="${TITLE:-Chaos Mesh}"
+DESCRIPTION="${DESCRIPTION:-Commit ${GIT_COMMIT}. Generated at $(date).}"
+
+cd chaos-mesh && gopages -brand-title "${TITLE}" -brand-description "${DESCRIPTION}" -base https://chaos-mesh.dev/godoc/$TARGET_BRANCH_OR_TAG -out $OUT_DIRECTORY
