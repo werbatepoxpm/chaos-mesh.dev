056ea6bbb5733bab3f90aae09c7b58d74f7e6de4
STRRL <str_ruiling@outlook.com>
2022-01-16 17:24:46 +0800

    chore: update scripts
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- image/chaos-mesh.dev-godoc/entry-point.sh
+++ image/chaos-mesh.dev-godoc/entry-point.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 TARGET_BRANCH_OR_TAG="${1:-master}"
+TITLE="${TITLE:-Chaos Mesh}"
+DESCRIPTION="${DESCRIPTION:-Updated at $(date)}"
 
 echo "target branch: $TARGET_BRANCH_OR_TAG"
 echo "current directory: $(pwd)"
@@ -8,4 +10,4 @@ git clone --depth 1 --branch $TARGET_BRANCH_OR_TAG https://github.com/chaos-mesh
 
 OUT_DIRECTORY="$(pwd)/out/$TARGET_BRANCH_OR_TAG"
 mkdir -p $OUT_DIRECTORY
-cd chaos-mesh && gopages -base https://chaos-mesh.dev/godoc/$TARGET_BRANCH_OR_TAG -out $OUT_DIRECTORY
+cd chaos-mesh && gopages -brand-title ${TITLE} -brand-description ${DESCRIPTION} -base https://chaos-mesh.dev/godoc/$TARGET_BRANCH_OR_TAG -out $OUT_DIRECTORY
