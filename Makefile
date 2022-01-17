a577d7b8a3e3c25f95e0680a8f4db87113515722
STRRL <str_ruiling@outlook.com>
2022-01-17 22:52:44 +0800

    chore: godoc with google analytics intergration
    Signed-off-by: STRRL <str_ruiling@outlook.com>

--- Makefile
+++ Makefile
@@ -2,17 +2,18 @@
 all: clean godoc api-reference
 
 .PHONY: godoc
-godoc-master:
+godoc:
 	mkdir -p out/godoc
 	docker run -v $(shell pwd)/out/godoc:/playground/out ghcr.io/strrl/chaos-mesh.dev-godoc:latest master
 
 .PHONY: api-reference
 api-reference:
+	mkdir -p out/reference
+	docker run -v $(shell pwd)/out/reference:/playground/out ghcr.io/strrl/chaos-mesh.dev-reference:latest master
 
 .PHONY: clean
 clean:
 	rm -rf out
-
 
 .PHONY: image
 image: image-godoc image-reference
--- image/chaos-mesh.dev-godoc/entry-point.sh
+++ image/chaos-mesh.dev-godoc/entry-point.sh
@@ -15,4 +15,4 @@ mkdir -p $OUT_DIRECTORY
 TITLE="${TITLE:-Chaos Mesh}"
 DESCRIPTION="${DESCRIPTION:-Commit ${GIT_COMMIT}. Generated at $(date).}"
 
-cd chaos-mesh && gopages -brand-title "${TITLE}" -brand-description "${DESCRIPTION}" -base https://chaos-mesh.dev/godoc/$TARGET_BRANCH_OR_TAG -out $OUT_DIRECTORY
+cd chaos-mesh && gopages -brand-title "${TITLE}" -brand-description "${DESCRIPTION}" -google-analytics "G-7DESCBGPBX" -base https://chaos-mesh.dev/godoc/$TARGET_BRANCH_OR_TAG -out $OUT_DIRECTORY
